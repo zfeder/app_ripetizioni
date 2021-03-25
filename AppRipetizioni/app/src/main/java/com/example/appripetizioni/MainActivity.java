d1edc93e5ea1aaf9a93d853d16f57b44969e9da4
Giorgio <giorgio.licitra264@edu.unito.it>
2021-03-25 16:05:40 +0100

    prova 2

--- AppRipetizioni/app/src/main/java/com/example/appripetizioni/MainActivity.java
+++ AppRipetizioni/app/src/main/java/com/example/appripetizioni/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.appripetizioni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //questa fkljòmf
+
+
+    public void login(View view) {
+        TextView username = findViewById(R.id.editText1);
+        TextView password = findViewById(R.id.editText2);
+        String usernameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+        Log.d(usernameString, passwordString);
+
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+
+            //correcct password
+        } else {
+            //wrong password
+        }
+    }
+
 }
